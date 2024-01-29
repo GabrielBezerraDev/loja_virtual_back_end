beb1fe4350f8fdac7593ad255dd5ceb2f2d6ffae
root <root@Bezerra>
2024-01-28 20:31:06 -0400

    [UPDATE] - Adicionando novos arquvios sql para automatizar o db e organizar

--- mysql.sh
+++ mysql.sh
@@ -1,2 +1,6 @@
-docker cp ./templateDatas.sql mysql:/templateDatas.sql
-docker exec -it mysql bash -c "mysql -u root --password=root nest < ./templateDatas.sql"
+docker cp ./EquipamentoDatas.sql mysql:/EquipamentoDatas.sql && docker cp ./EsporteDatas.sql mysql:/EsporteDatas.sql
+docker cp ./Cosmeticos.sql mysql:/Cosmeticos.sql && docker cp ./Suplementos.sql mysql:/Suplementos.sql
+docker exec -it mysql bash -c "mysql -u root --password=root nest < ./EsporteDatas.sql"
+docker exec -it mysql bash -c "mysql -u root --password=root nest < ./EquipamentoDatas.sql"
+docker exec -it mysql bash -c "mysql -u root --password=root nest < ./Cosmeticos.sql"
+docker exec -it mysql bash -c "mysql -u root --password=root nest < ./Suplementos.sql"
